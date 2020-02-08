03c5c8a35e801bf3a61544f2d7c9bf68ee78a577
Trevor Manz <trevor.j.manz@gmail.com>
2020-02-08 17:14:54 -0500

    fix ref error

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ const selection = [
   const z = await zarr.openArray(config);
 
   console.log('.get')
-  const t2 = tf.tensor((await z.get(selection)).flatten(), raw.shape);
-  t2.print();
+  const nestedArray = await z.get(selection);
+  const t = tf.tensor(nestedArray.flatten(), nestedArray.shape);
+  t.print();
 
   console.log('.getRaw')
   const [data, shape] = await z.getRaw(selection);
-  const t = tf.tensor(data, shape);
-  t.print();
+  const t2 = tf.tensor(data, shape);
+  t2.print();
 })();
